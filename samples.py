c0dddc2ffa100e3dc6c72d2a4259124d6fa92ed3
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2020-03-17 20:51:28 -0500

    Create samples.py
    samples de python

---
+++ samples.py
@@ -0,0 +1,8 @@
+import math #Esta es la libreria que incluye funciones matemáticas.
+#debes revisar la documentación para más detalles
+#numpty matplotlib scikit opencv
+valor=9
+resultado=math.factorial(valor)
+print("el resultado es: ",resultado)
+#print(math.gcd(18,24))
+#print(math.exp(3))
